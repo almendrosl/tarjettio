7d929b173a4091e495ae58736a909b273df27ac2
lucas <almendroslm@gmail.com>
2025-07-19 19:55:12 -0300

    Init project

---
+++ src/main/java/com/tarjettio/tarjettio/TarjettioApplication.java
@@ -0,0 +1,13 @@
+package com.tarjettio.tarjettio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarjettioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarjettioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarjettio/tarjettio/TarjettioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarjettio.tarjettio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarjettioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
